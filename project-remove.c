102863ea31c188b73bd094a3d22fd5aad790c542
Vania <vaniajavadii@gmail.com>
2026-08-10 03:41:14 +0330

    Update project-remove.c

--- project-remove.c
+++ project-remove.c
@@ -1,49 +1,32 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <string.h>
-#include <ctype.h>
-#include <stdlib.h>
-//#include <cstdlib>
-//#include <bits/stdc++.h>
-//#include <iostream>
-//fgets(line2, 2000, stdin);
-//int size_line1 = strlen(line1) - 1; 
-typedef long long ll;
-
-//using namespace std;
+#include "shoro.h"
 
 
-// ////////////////////////////////////////////////////////student
-void remove_student(Student* st){
-	if(st == headStudent){
-		headStudent = st->next;
-		if(headStudent != NULL) 	headStudent->prev = NULL;
-		return;
-	}
-
-	if(st->prev != NULL){
-		st->prev->next = st->next;
-	}
-	if(st->next != NULL){
-		st->next->prev = st->prev;
-	}
-	free(st);
-	return;
+void remove_student(Student* st) {
+    if (st == NULL) return;
+    st->active = 0;
+    reset_file_student();
 }
-///////////////////////////////////////////////////////////request
-void remove_request(Request* rqst){
-	if(rqst == headRequest){
-		headRequest = rqst->next;
-		if(headRequest != NULL) 	headRequest->prev = NULL;
-		return;
-	}
 
-	if(rqst->prev != NULL){
-		rqst->prev->next = rqst->next;
-	}
-	if(rqst->next != NULL){
-		rqst->next->prev = rqst->prev;
-	}
-	free(rqst);
-	return;
+
+void remove_faculty(Faculty* fc) {
+    if (fc == NULL) return;
+    fc->active = 0;
+    reset_file_faculty();
+}
+
+void remove_request(Request* rqst) {
+    if (rqst == NULL) return;
+
+    if (rqst == headRequest) {
+        headRequest = rqst->next;
+        if (headRequest != NULL) headRequest->prev = NULL;
+        free(rqst->address);
+        free(rqst);
+        return;
+    }
+
+    if (rqst->prev != NULL) rqst->prev->next = rqst->next;
+    if (rqst->next != NULL) rqst->next->prev = rqst->prev;
+    free(rqst->address);
+    free(rqst);
 }
