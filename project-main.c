7059f4a60088fbe09ffb260fc2e798e16efda7ab
Vania <vaniajavadii@gmail.com>
2026-08-10 03:38:41 +0330

    Update project-main.c

--- project-main.c
+++ project-main.c
@@ -1,34 +1,10 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <string.h>
-#include <ctype.h>
-#include <stdlib.h>
-//#include <cstdlib>
-//#include <bits/stdc++.h>
-//#include <iostream>
-//fgets(line2, 2000, stdin);
-//int size_line1 = strlen(line1) - 1; 
-typedef long long ll;
+#include "shoro.h"
 
-//using namespace std;
-
-/////////////////////////////////////////////////////////////// GLOBAL FUNC
-Admin admin = { "admin", "aaaaa" };
-Term term = {{0,0,0,0}, -1};
-Student* headStudent = NULL;
-Request* headRequest = NULL;
-Offer* headOffer = NULL;
-Faculty* headFaculty = NULL;
-
-
-/////////////////////////////////////////////////////////////////main
-
-int main(){
-    read_student();
-    read_faculty();
+int main(void) {
     read_course();
+    read_faculty();
+    read_offering();
+    read_student();
     login_menu();
-
     return 0;
 }
-
