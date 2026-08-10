5ca03cacae144859cf2acfe8adc57714cf846eb1
Vania <vaniajavadii@gmail.com>
2026-08-10 03:43:33 +0330

    Update project-admin.c

--- project-admin.c
+++ project-admin.c
@@ -1,339 +1,296 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <string.h>
-#include <ctype.h>
-#include <stdlib.h>
-//#include <cstdlib>
-//#include <bits/stdc++.h>
-//#include <iostream>
-//fgets(line2, 2000, stdin);
-//int size_line1 = strlen(line1) - 1; 
-typedef long long ll;
+#include "shoro.h"
 
-//using namespace std;
-//////////////////////////////////////////////////////////////calendar
-char* calendar_message(int state){
-	if(state == 1)	return "enabled";
-	else return"disabled";
+//////////////////////////////////////////////////////////////////////////////////////////////////calendar
+char* calendar_message(int state) {
+    return (state == 1) ? "enabled" : "disabled";
 }
 
-void admin_calendar(){
-	if(term->id == -1){
-		prinf("Enter new term id:");
-		scanf("%d%*c", &term->id);
-	}
-	else{
-		printf("term %d", term->id);
-	}
-
-	printf("1.offering: %s\n
-		2.unit selection: %s\n
-		3. class & exams: %s\n
-		4,grade recording: %s\n
-		5. go to main menu: %s\n
-		Enter a time to trigger:", 
-		calendar_message(term->state[0]), 
-		calendar_message(term->state[1]),
-		calendar_message(term->state[2]),
-		calendar_message(term->state[3]));
-
-	int option;	scanf("%d%*c", &option);
-	if(option == 5)	return;
-	else if(term->state[--option] == 2){
-		printf("not possible");
-	}
-	else if(term->state[option] == 1){
-		if(!option || term->state[option - 1] == 2){
-			term->state[option] = 2;
-		}
-		else
-			printf("not possible");
-
-	}
-	else{
-		if(!option || term->state[option - 1])
-			term->state[option] = 1;
-		else
-			printf("not possible");
-	}
-
-
-	admin_calendar();
-	return;
-
-}
-/////////////////////////////////////////////////////////////////////////////////////////Studentlist
-
-
-void search_student_menu(){
-	printf("1.search by first name\n
-		2. search by lastname\n
-		3. search by student id\n
-		4.back\n
-		........\n
-		Enter an option: ");
-	int option;
-	char* phrase;
-	Student* st;
-	scanf("%d%*c", &option);
-
-	if(option == 4) return;
-	if(option == 3){
-		ll studentId;
-		scanf("%lld%*c", %studentId);
-		st = create_student(NULL, NULL, NULL, NULL, NULL,
-                                                 NULL, NULL,
-                                                 NULL, NULL, NULL,
-                                                 0.0, username, 0, 0, 
-                                                 NULL, NULL, NULL);
-	}
-	else{
-		size_t bufferSize = 0;
-		getline(&phrase, &bufferSize, stdin);
-		phrase[strscpn(line, "\n")] = '\0';
-
-		if(option == 1)
-			st = create_student(phrase, NULL, NULL, NULL, NULL,
-                                                 NULL, NULL,
-                                                 NULL, NULL, NULL,
-                                                 0.0, 0, 0, 0, 
-                                                 NULL, NULL, NULL);
-		else
-			st = create_student(NULL, phrase, NULL, NULL, NULL,
-                                                 NULL, NULL,
-                                                 NULL, NULL, NULL,
-                                                 0.0, 0, 0, 0, 
-                                                 NULL, NULL, NULL);
-	}
-
-	bool flag = 0;
-	Student* foundedSt = search_student(headStudent, st);
-	print_student(foundedSt);
-
-	while(foundedSt != NULL){
-		foundedSt = search_student(foundedSt->next, st);
-		print_student(foundedSt);
-	}
-	return;
-
-}
-
-
-void student_list(){
-	printf("STUDENT LIST\n
-		|first name |last name |student id |national code |field |entrance year |section |mentor |department| answer 1 | answer 2 | answer 3|\n
-		|----------|----------|----------|---------------|------|--------------|-------|------|----------|----------|----------|----------|\n");
-	print_student_list(headStudent);
-	printf("1.search\n
-		2.go back\n
-		3.enter an option:");
-
-	int option;
-	scanf("%d%*c", option);
-	if(option == 1)
-		search_student_menu();
-	return;
-}
-
-
-///////////////////////////////// register_student_menu
-
-
-void register_student_menu(){
-	prinf("Register student(s)\n
-	 1. Register one student\n
-	 2. register a group of students (import a file)\n 
-	 3. back\n 
-	 Enter an option ");
-	int option = 0;
-	scanf("%d%*c", option);
-	if(option == 3)	return;
-
-	char* line = NULL;
-    size_t bufferSize = 0;
-    getline(&line, &bufferSize, stdin);
-    line[strscpn(line, "\n")] = '\0';
-
-    if(option == 1){
-    	printf("Enter student info");
-    	getline(&line, &bufferSize, stdin);
-    	add_student(line);
-    	free(line);
+void admin_calendar(void) {
+    if (term.id == -1) {
+        printf("Enter new term id: ");
+        scanf("%lld%*c", &term.id);
+    } else {
+        printf("Term %lld\n", term.id);
     }
-    else{
-    	printf("Enter file address");
 
-    	getline(&line, &bufferSize, stdin);
-    	line[strscpn(line, "\n")] = '\0';
-    	FILE *fp = fopen(line, "r");
+    printf("1. offering: %s\n2. unit selection: %s\n3. class & exams: %s\n4. grade recording: %s\n5. go to main menu\nEnter a time to trigger: ",
+        calendar_message(term.state[0]), calendar_message(term.state[1]),
+        calendar_message(term.state[2]), calendar_message(term.state[3]));
 
-
-    	ssize_t read = getline(&line, &bufferSize, fp);
-    	while(read != -1){
-    		add_student(line);
-    		ssize_t read = getline(&line, &bufferSize, fp);
-    	}
-    	free(line);
-    	fclose(fp);
-    }
-    register_student_menu();
-    return;
-}	
-
-////////////////////////////////////////////remove student menu
-
-void remove_student_menu(){
-	prinf("1.Remove student\n2.back\nEnter an option:");
-	int option;
-	scanf("%d%*c", &option);
-	if(option == 2)
-		return
-
-	printf("Enter student id:")
-	ll id;
-	scanf("%lld%*c", &id);
-
-	Student* ptr = search_student(headStudent, create_student(NULL, NULL, NULL, NULL, NULL,
-                                                 NULL, NULL,
-                                                 NULL, NULL, NULL,
-                                                 0.0, id, 0, 0, 
-                                                 NULL, NULL, NULL));
-
-	if(ptr == NULL){
-		printf("student doesnt exist");
-		return;
-	}
-
-	printf("|%s |%s |%lld |%lld |%s |%lld |%s |%s |%s|\nRemove student? [y/n]",
-			ptr->name, ptr->lastname, ptr->id, ptr->kodemeli, ptr->reshte,
-			ptr->year, ptr->maghta, ptr->ostadrahnama, ptr->department);
-
-	char ch;
-	scanf("%c", &ch);
-	if(ch == 'y'){
-		remove_student(ptr);
-		reset_file_student();
-	}
-	return;
-}
-//////////////////////////////////////////// admin_student
-void admin_students() {
-	int option = 0;
-    printf("1. students list\n"
-           "2. register student(s)\n"
-           "3. remove student(s)\n"
-           "4. back to admin dashboard\n"
-           "Enter an option: ");
-    
+    int option;
     scanf("%d%*c", &option);
+    if (option == 5) return;
+
+    option--; 
+    if (option < 0 || option > 3) { printf("Not possible.\n"); admin_calendar(); return; }
+
+    if (term.state[option] == 2) {
+        printf("Not possible.\n");
+    }
+    else if (term.state[option] == 1) {
+   
+        if (option == 0 || term.state[option - 1] == 2) term.state[option] = 2;
+        else printf("Not possible.\n");
+    }
+    else {
+      
+        if (option == 0 || term.state[option - 1] != 0) term.state[option] = 1;
+        else printf("Not possible.\n");
+    }
+
+    admin_calendar();
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////student
+
+void search_student_menu(void) {
+    printf("1. Search by first name\n2. Search by last name\n3. Search by student id\n4. back\nEnter an option: ");
+    int option;
+    scanf("%d%*c", &option);
+    if (option == 4) return;
+
+    Student* key;
+    if (option == 3) {
+        printf("Enter student id: ");
+        ll studentId; scanf("%lld%*c", &studentId);
+        key = creat_student(NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0.0, studentId, 0, 0, 1, NULL, NULL, NULL);
+    } else {
+        printf("The phrase to search: ");
+        char* line = NULL; size_t bufferSize = 0;
+        getline(&line, &bufferSize, stdin);
+        line[strcspn(line, "\r\n")] = '\0';
+        if (option == 1)
+            key = creat_student(line, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0.0, 0, 0, 0, 1, NULL, NULL, NULL);
+        else
+            key = creat_student(NULL, line, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0.0, 0, 0, 0, 1, NULL, NULL, NULL);
+        free(line);
+    }
+
+    Student* found = search_student(headStudent, key);
+    int count = 0;
+    while (found != NULL) {
+        print_student(found);
+        count++;
+        found = search_student(found->next, key);
+    }
+    if (count == 0) printf("No students found.\n");
+
+    search_student_menu();
+}
+
+void student_list(void) {
+    printf("Student List\n");
+    printf("|first name |last name |student id |national code |field |entrance year |section |mentor |department| answer 1 | answer 2 | answer 3|\n");
+    printf("|-----------|----------|-----------|--------------|------|--------------|--------|-------|----------|----------|----------|----------|\n");
+    print_student_list(headStudent);
+
+    printf("1. search\n2. go back\nEnter an option: ");
+    int option; scanf("%d%*c", &option);
+    if (option == 1) search_student_menu();
+}
+//// name, last name, id, password, kodemeli, reshte, year, maghta, ostadrahnama, department, answer1, answer2, answe3
+void register_student_menu(void) {
+    printf("Register student(s)\n1. Register one student\n2. Register a group of students (import a file)\n3. back\nEnter an option: ");
+    int option; scanf("%d%*c", &option);
+    if (option == 3) return;
+
     if (option == 1) {
-        student_list();
-    } 
+        printf("Enter student info: ");
+        char* line = NULL; size_t bufferSize = 0;
+        getline(&line, &bufferSize, stdin);
+        add_student(line);
+        free(line);
+    }
     else if (option == 2) {
-        register_student_menu();
-    } 
-    else if (option == 3) {
-        remove_student_menu();
-    } 
-    else
+        printf("Enter file name or address: ");
+        char filename[256];
+        fgets(filename, sizeof(filename), stdin);
+        filename[strcspn(filename, "\r\n")] = '\0';
+
+        FILE* fp = fopen(filename, "r");
+        if (fp == NULL) {
+            printf("Could not open that file.\n");
+        } else {
+            char* line = NULL; size_t bufferSize = 0; ssize_t nread;
+            while ((nread = getline(&line, &bufferSize, fp)) != -1) {
+                if (line[0] == '\n' || line[0] == '\0') continue;
+                add_student(line);
+            }
+            free(line);
+            fclose(fp);
+        }
+    }
+
+    register_student_menu();
+}
+
+void remove_student_menu(void) {
+    printf("Remove student(s)\nEnter student id: ");
+    ll id; scanf("%lld%*c", &id);
+
+    Student* key = creat_student(NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0.0, id, 0, 0, 1, NULL, NULL, NULL);
+    Student* ptr = search_student(headStudent, key);
+
+    if (ptr == NULL) {
+        printf("Student doesn't exist.\n");
         return;
-}
-//////////////////////////////////////////////admin_faculty_members
+    }
 
-void admin_faculty_members(){
-
-}
-
-
-////////////////////////////////////////////request
-
-void accept_request(Request* rqst){
-	if(rqst == NULL){
-		printf("there is no request with that number\n");
-		return;
-	}
-	if(ter[1] == 2){
-		printf("NOT POSSIBLE! ENTEKHAB VAHED TAMOM SHODE");
-
-	}
-
-	else if(rqst->type == 0){
-		if(term[1] == 2){
-			printf("NOT POSSIBLE!");
-		}
-		else{
-			Offer* offer = creat_offer(rqst->course, rqst->course->daneshkade, rqst->address,
-                    rqst->faculty, term->id, 0, rqst->enroll,
-                    NULL, NULL, NULL);
-			push_offer(offer, headOffer);
-			push_offer_file(offer);
-		}	
-	}
-	else if(rqst->type == 1){
-		remove_offer(rqst->offer);
-		reset_offer();
-	}
-	else if(rqst->type == 2){
-		rqst->offer->zarfiyat = rqst->enroll;
-		reset_offer();
-	}
-
-	remove_request(rqst);
+    print_student(ptr);
+    printf("Remove student? [y/n] ");
+    char ch; scanf(" %c%*c", &ch);
+    if (ch == 'y' || ch == 'Y') {
+        remove_student(ptr);
+        printf("Done.\n");
+    }
 }
 
-void admin_request(){
-	printf("List of requests:\n");
-	print_request_list(1, headRequest);
-	printf("1. accept request\n2. Go back\nEnter an option:");
-	int option;	scanf("%d%*c", &option);
-	if(option == 2)	return;
-	printf("Enter request's number: ");
-	scanf("%d%*c", &option);
-	accept_request(search_request(option, 1, headRequest));
-	admin_request();
-	return;
+void admin_students(void) {
+    printf("1. students list\n2. register student(s)\n3. remove student(s)\n4. back to admin dashboard\nEnter an option: ");
+    int option; scanf("%d%*c", &option);
 
-}
-////////////////////////////////////////////////////////////offering
-void admin_offering(){
+    if (option == 1) student_list();
+    else if (option == 2) register_student_menu();
+    else if (option == 3) remove_student_menu();
+    else return;
 
+    admin_students();
 }
 
-///////////////////////////////////////////////////////////courses
+//////////////////////////////////////////////////////////////////////////////////////////////////faculity
+void search_faculty_menu(void) {
+    printf("1. Search by first name\n2. Search by last name\n3. Search by username\n4. back\nEnter an option: ");
+    int option;
+    scanf("%d%*c", &option);
+    if (option == 4) return;
 
+    Faculty* key;
+    if (option == 3) {
+        printf("Enter username: ");
+        char* line = NULL; size_t bufferSize = 0;
+        getline(&line, &bufferSize, stdin);
+        line[strcspn(line, "\r\n")] = '\0';
+        key = creat_faculty(NULL, NULL, NULL, NULL, NULL, line, 0, NULL, 1, NULL, NULL);
+        free(line);
+    } else {
+        printf("The phrase to search: ");
+        char* line = NULL; size_t bufferSize = 0;
+        getline(&line, &bufferSize, stdin);
+        line[strcspn(line, "\r\n")] = '\0';
+        if (option == 1)
+            key = creat_faculty(line, NULL, NULL, NULL, NULL, NULL, 0, NULL, 1, NULL, NULL);
+        else
+            key = creat_faculty(NULL, line, NULL, NULL, NULL, NULL, 0, NULL, 1, NULL, NULL);
+        free(line);
+    }
 
+    Faculty* found = search_faculty(headFaculty, key);
+    int count = 0;
+    while (found != NULL) {
+        print_faculty(found);
+        count++;
+        found = search_faculty(found->next, key);
+    }
+    if (count == 0) printf("No faculty members found.\n");
 
-////////////////////////////////////////////////////////////dashboard
-void dashboard_admin(){
-	printf("Welcom %s\n
-		1.calendar\n
-		2.students\n
-		3.faculity members\n
-		4.requests\n
-		5.offerings\n
-		6.courses\n
-		7.Log out\n
-		Enter an option:", admin->username);
+    search_faculty_menu();
+}
 
-	int option = 0;
-	scanf("%d%*c", &option);
-	 if (option == 1) {
-	    admin_calendar();
-	} 
-	else if (option == 2) {
-	    admin_students();
-	} 
-	else if (option == 3) {
-	    admin_faculty_members();
-	} 
-	else if (option == 4) {
-	    admin_request();
-	} 
-	else if (option == 5) {
-	    admin_offerings();
-	} 
-	else if (option == 6) {
-	    admin_courses();
-	} 
-	else return;
+void faculty_list(void) {
+    printf("Faculty List\n");
+    printf("|first name |last name |username |field |last degree |national code |department|\n");
+    printf("|-----------|----------|---------|------|------------|--------------|----------|\n");
+    print_faculty_list(headFaculty);
+
+    printf("1. search\n2. go back\nEnter an option: ");
+    int option; scanf("%d%*c", &option);
+    if (option == 1) search_faculty_menu();
+}
+
+void register_faculty_menu(void) {
+    printf("Register faculty member(s)\n1. Register one faculty member\n2. Register a group (import a file)\n3. back\nEnter an option: ");
+    int option; scanf("%d%*c", &option);
+    if (option == 3) return;
+
+    if (option == 1) {
+        printf("Enter faculty info: ");
+        char* line = NULL; size_t bufferSize = 0;
+        getline(&line, &bufferSize, stdin);
+        add_faculty(line);
+        free(line);
+    }
+    else if (option == 2) {
+        printf("Enter file name or address: ");
+        char filename[256];
+        fgets(filename, sizeof(filename), stdin);
+        filename[strcspn(filename, "\r\n")] = '\0';
+
+        FILE* fp = fopen(filename, "r");
+        if (fp == NULL) {
+            printf("Could not open that file.\n");
+        } else {
+            char* line = NULL; size_t bufferSize = 0; ssize_t nread;
+            while ((nread = getline(&line, &bufferSize, fp)) != -1) {
+                if (line[0] == '\n' || line[0] == '\0') continue;
+                add_faculty(line);
+            }
+            free(line);
+            fclose(fp);
+        }
+    }
+
+    register_faculty_menu();
+}
+
+void remove_faculty_menu(void) {
+    printf("Remove faculty member(s)\nEnter username: ");
+    char* username = NULL; size_t bufferSize = 0;
+    getline(&username, &bufferSize, stdin);
+    username[strcspn(username, "\r\n")] = '\0';
+
+    Faculty* key = creat_faculty(NULL, NULL, NULL, NULL, NULL, username, 0, NULL, 1, NULL, NULL);
+    Faculty* ptr = search_faculty(headFaculty, key);
+    free(username);
+
+    if (ptr == NULL) {
+        printf("Faculty member doesn't exist.\n");
+        return;
+    }
+
+    print_faculty(ptr);
+    printf("Remove faculty member? [y/n] ");
+    char ch; scanf(" %c%*c", &ch);
+    if (ch == 'y' || ch == 'Y') {
+        remove_faculty(ptr);
+        printf("Done\n");
+    }
+}
+
+void admin_faculty_members(void) {
+    printf("1. faculty list\n2. register faculty member(s)\n3. remove faculty member(s)\n4. back to admin dashboard\nEnter an option: ");
+    int option; scanf("%d%*c", &option);
+
+    if (option == 1) faculty_list();
+    else if (option == 2) register_faculty_menu();
+    else if (option == 3) remove_faculty_menu();
+    else return;
+
+    admin_faculty_members();
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////dashboard
+void dashboard_admin(void) {
+    printf("Admin dashboard:\nWelcome %s\n1. Calendar\n2. Students\n3. Faculty members\n4. Requests\n5. Offerings\n6. Courses\n7. Log out\nEnter an option: ",
+        admin.username);
+
+    int option; scanf("%d%*c", &option);
+    if (option == 1) admin_calendar();
+    else if (option == 2) admin_students();
+    else if (option == 3) admin_faculty_members();
+    else if (option == 4) admin_request();
+    else if (option == 5) admin_offerings();
+    else if (option == 6) admin_courses();
+    else return;
+
+    dashboard_admin();
 }
